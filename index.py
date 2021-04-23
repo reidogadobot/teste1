4dbab3c220b956ce6197a0930970016098130002
reidogadobot <79222246+reidogadobot@users.noreply.github.com>
2021-04-23 13:08:11 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,5 @@
+print('Olá, seja bem vindo')
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite outro número: '))
+s = n1 + n2
+print('A soma entre {} e {} é {}'. format(n1, n2, s))
